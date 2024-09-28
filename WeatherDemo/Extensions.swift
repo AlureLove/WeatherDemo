cb4fdc58371ad90b9d0abc3c347dd661d52ee42e
AlureLove <qhlrx@qq.com>
2024-09-28 23:13:40 +0900

    feat: add weather api
    add weather api and update weather view

---
+++ WeatherDemo/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  WeatherDemo
+//
+//  Created by JethroLiu on 2024/9/28.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
---
+++ WeatherDemo/Views/WeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherView.swift
+//  WeatherDemo
+//
+//  Created by JethroLiu on 2024/9/28.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather: ResponseBody
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView(weather: previewWeather)
+}
